ff2e6eb98e4a938ea74499ee82e5696cc87fc213
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-11-13 14:28:21 +0300

    run spotless

--- app/src/test/java/com/mzazi/cashregister/presentation/MainScreenViewModelTest.kt
+++ app/src/test/java/com/mzazi/cashregister/presentation/MainScreenViewModelTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 CashRegister
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mzazi.cashregister.presentation
 
 import app.cash.turbine.test
@@ -12,7 +27,6 @@ import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.Test
-
 
 @ExperimentalCoroutinesApi
 class MainScreenViewModelTest {
@@ -41,6 +55,5 @@ class MainScreenViewModelTest {
         viewModel.summation.test {
             Truth.assertThat(awaitItem()).isEqualTo("23")
         }
-
     }
 }
